d010463be0f7b0ce993284c86a0b97717be1f707
CuriousCod <mail@curiouscod.com>
2021-04-06 20:07:05 +0300

    Program attemps to close the LCore window after starting the process.

--- RestartLCore/MainWindow.xaml.cs
+++ RestartLCore/MainWindow.xaml.cs
@@ -24,8 +24,16 @@ namespace RestartLCore
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class RestartProgram
+        {
+            public string name { get; set; }
+            public string path { get; set; }
+            public string windowName { get; set; }
+
+        }
 
         private System.Windows.Forms.NotifyIcon notifyIcon;
+        private RestartProgram restartable = new RestartProgram();
 
         public MainWindow()
         {
@@ -36,6 +44,11 @@ namespace RestartLCore
 
             // Initialize notification area features
             InitializeNotificationArea();
+
+            // Init restartable program object
+            restartable.name = "LCore";
+            restartable.path = @"C:\Program Files\Logitech Gaming Software\LCore.exe";
+            restartable.windowName = "Logitech Gaming Software";
 
         }
 
@@ -54,7 +67,7 @@ namespace RestartLCore
                     break;
                 case SessionSwitchReason.SessionUnlock:
                     // Back from lock/standby
-                    RestartProcess("LCore", @"C:\Program Files\Logitech Gaming Software\Lcore.exe");
+                    RestartProcess(restartable);
                     break;
                 default:
                     break;
@@ -65,7 +78,7 @@ namespace RestartLCore
         {
             if (e.ChangedButton == MouseButton.Left)
             {
-                RestartProcess("LCore", @"C:\Program Files\Logitech Gaming Software\Lcore.exe");
+                RestartProcess(restartable);
             }
         }
 
@@ -101,5 +114,57 @@ namespace RestartLCore
             if (notifyIcon != null)
                 notifyIcon.ShowBalloonTip(2000);
         }
+
+        async private void RestartProcess(RestartProgram restartable)
+        {
+            buttonRestartProgram.IsEnabled = false;
+
+            await Task.Run(() =>
+            {
+                ProcessHandling procHandle = new ProcessHandling();
+
+                List<string> killedProcs = procHandle.KillProcess(restartable.name);
+
+                if (killedProcs.Count == 0)
+                {
+
+                    // Required when updating UI from other thread
+                    Dispatcher.Invoke(() =>
+                    {
+                        AppendLog($"Could not kill {restartable.name}");
+                    });
+                    
+                }
+                else
+                {
+                    foreach (string killedProc in killedProcs)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            AppendLog($"{killedProc} killed");
+                        });   
+                    }
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    AppendLog($"Starting process {restartable.path}");
+                });
+                
+
+                procHandle.StartProcess(restartable.path);
+
+                string results = procHandle.CloseAppWindow(restartable.windowName);
+
+                Dispatcher.Invoke(() =>
+                {
+                    AppendLog(results);
+                    buttonRestartProgram.IsEnabled = true;
+                });
+
+            });
+
+        }
+
     }
 }
--- RestartLCore/ProcessHandling.cs
+++ RestartLCore/ProcessHandling.cs
@@ -2,34 +2,45 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace RestartLCore
 {
-    public partial class MainWindow : Window
+    public class ProcessHandling
     {
-        public void RestartProcess(string proc, string procLocation)
-        {
-            KillProcess(proc);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
-            AppendLog($"Starting process {procLocation}");
-            StartProcess(procLocation);
-        }
+        /// <summary>
+        /// Find window by Caption only. Note you must pass IntPtr.Zero as the first parameter.
+        /// </summary>
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
 
-        public void KillProcess(string procName)
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        const UInt32 WM_CLOSE = 0x0010;
+
+        public List<string> KillProcess(string procName)
         {
             Process[] procs = Process.GetProcessesByName(procName);
+            List<string> kills = new List<string>();
 
             foreach (Process proc in procs)
             {
                 if (proc.ProcessName == procName)
                 {
                     proc.Kill();
-                    AppendLog($"{proc.ProcessName} killed");
+                    kills.Add(procName);
                 }
             }
+
+            return kills;
         }
 
         public void StartProcess(string procName)
@@ -37,6 +48,44 @@ namespace RestartLCore
             Process proc = new Process();
             proc.StartInfo.FileName = procName;
             proc.Start();
+
+        }
+
+        public string CloseAppWindow(string procWindowName)
+        {
+            IntPtr windowPtr = IntPtr.Zero;
+
+            int counter = 0;
+
+            // Wait for the window to open
+            while (windowPtr == IntPtr.Zero)
+            {
+                windowPtr = FindWindowByCaption(IntPtr.Zero, procWindowName);
+
+                Thread.Sleep(500);
+
+                // Prevent infinite loop, in case window is not found in the set time period
+                if (counter > 10)
+                {
+                    break;
+                }
+                counter += 1;
+            }
+
+            if (windowPtr != IntPtr.Zero)
+            {
+                // Wait for startup
+                // TODO There should be a better way to determine when the window has initialized
+                Thread.Sleep(2000);
+
+                // Need to grab the window one more time after it has initialized
+                windowPtr = FindWindowByCaption(IntPtr.Zero, procWindowName);
+
+                SendMessage(windowPtr, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                return $"Attempted to close window {procWindowName}";
+            }
+
+            return $"Could not close window {procWindowName}";
         }
     }
 }
